a714868a0c2a28d19aa2c29c761144ef159606d5
jeaunrg <jeanunrug@gmail.com>
2022-09-03 01:11:36 +0200

    add nodes

--- src/controller/main_controller.py
+++ src/controller/main_controller.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from PyQt5 import QtWidgets
 from src.controller.controllers import LoadImageWC, LoadTextWC
 from src.model.main_model import MainModel
 from src.view.main_view import GraphItem, GraphView, MainView
@@ -8,6 +9,12 @@ from src.view.main_view import GraphItem, GraphView, MainView
 class WidgetEnum(Enum):
     load_im = "load_im"
     load_txt = "load_txt"
+
+
+WIDGET_NAME_DICT = {
+    "load:image": WidgetEnum.load_im,
+    "load:text": WidgetEnum.load_txt,
+}
 
 
 def widget_controller_factory(widget_name: WidgetEnum):
@@ -23,19 +30,39 @@ class MainController:
         self.view = MainView()
         self.make_connections()
         self.add_tab()
-        self.add_widget(WidgetEnum.load_im)
 
     def make_connections(self):
-        tab_widget = self.view.centralWidget()
-        tab_widget.tabCloseRequested.connect(lambda index: tab_widget.removeTab(index))
-        tab_widget.cornerWidget().clicked.connect(lambda: self.add_tab())
+        self.view.centralWidget().tabCloseRequested.connect(
+            lambda index: self.close_tab(index)
+        )
+        self.view.centralWidget().cornerWidget().clicked.connect(lambda: self.add_tab())
 
     def add_tab(self):
-        graph_view = GraphView()
-        self.view.centralWidget().addTab(graph_view, "Graph")
+        graph_controller = GraphController()
+        self.view.centralWidget().addTab(graph_controller.view, "Graph")
+
+    def close_tab(self, index: int):
+        reply = self.view.popup_dialog("close_scene")
+        if reply == QtWidgets.QMessageBox.Yes:
+            self.view.centralWidget().removeTab(index)
+
+
+class GraphController:
+    def __init__(self):
+        self.view = GraphView()
+        self.make_connections()
+        self.widget_position = (0, 0)
+
+    def set_widget_position(self, x, y):
+        self.widget_position = (x, y)
+
+    def make_connections(self):
+        self.view.mouse_clicked.connect(lambda x, y: self.set_widget_position(x, y))
+        self.view.menu.activated.connect(
+            lambda activation_key: self.add_widget(WIDGET_NAME_DICT[activation_key])
+        )
 
     def add_widget(self, widget_name: WidgetEnum):
         widget_controller = widget_controller_factory(widget_name)
-        item = GraphItem(widget_controller.view)
-        current_graph = self.view.centralWidget().currentWidget()
-        current_graph.scene().addItem(item)
+        item = GraphItem(self.widget_position, widget_controller.view)
+        self.view.scene().addItem(item)
--- src/view/main_view.py
+++ src/view/main_view.py
@@ -1,4 +1,13 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+RIGHT_CLICK_MENU = {"load": {"image": {}, "text": {}}}
+POPUPS = {
+    "close_scene": (
+        "Are you sure to close this scene ?",
+        QMessageBox.Yes | QMessageBox.No,
+    )
+}
 
 
 class MainView(QtWidgets.QMainWindow):
@@ -6,22 +15,59 @@ class MainView(QtWidgets.QMainWindow):
         super().__init__()
         self.setWindowState(QtCore.Qt.WindowActive)
         self.setWindowTitle("NewFrancis")
+        self.resize(400, 400)
         tab_widget = QtWidgets.QTabWidget()
         tab_widget.setTabsClosable(True)
         new_tab_button = QtWidgets.QPushButton("+")
         tab_widget.setCornerWidget(new_tab_button)
         self.setCentralWidget(tab_widget)
 
+    def popup_dialog(self, popup_key: str):
+        question, responses = POPUPS[popup_key]
+        return QMessageBox.question(self, "", question, responses)
+
 
 class GraphView(QtWidgets.QGraphicsView):
+    mouse_clicked = QtCore.pyqtSignal(int, int)
+
     def __init__(self):
         super().__init__()
         scene = QtWidgets.QGraphicsScene()
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorViewCenter)
+        self.setViewportUpdateMode(QtWidgets.QGraphicsView.NoViewportUpdate)
         self.setScene(scene)
+        self.menu = Menu(RIGHT_CLICK_MENU)
+
+    def contextMenuEvent(self, event):
+        menu_position = QtGui.QCursor.pos()
+        mouse_position = self.mapToScene(self.mapFromGlobal(menu_position))
+        self.mouse_clicked.emit(mouse_position.x(), mouse_position.y())
+        self.menu.exec(menu_position)
+        return super().contextMenuEvent(event)
+
+
+class Menu(QtWidgets.QMenu):
+    activated = QtCore.pyqtSignal(str)
+
+    def __init__(self, menu_dict: dict):
+        super().__init__()
+        for key, values in menu_dict.items():
+            self.recursive_fill_menu(self, key, values, key)
+
+    def recursive_fill_menu(self, menu, key, values, activation_key):
+        if len(values) == 0:
+            action = menu.addAction(key)
+            action.triggered.connect(lambda: self.activated.emit(activation_key))
+        else:
+            submenu = menu.addMenu(key)
+            for key, values in values.items():
+                self.recursive_fill_menu(
+                    submenu, key, values, f"{activation_key}:{key}"
+                )
 
 
 class GraphItem(QtWidgets.QGraphicsRectItem):
-    def __init__(self, widget):
+    def __init__(self, position, widget):
         super().__init__(0, 0, 75, 40)
         proxy = QtWidgets.QGraphicsProxyWidget(self)
         self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
@@ -32,3 +78,4 @@ class GraphItem(QtWidgets.QGraphicsRectItem):
             QtWidgets.QGraphicsItem.ItemIsMovable
             | QtWidgets.QGraphicsItem.ItemIsSelectable
         )
+        self.moveBy(*position)
